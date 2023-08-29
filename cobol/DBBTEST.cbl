67b95a2c76b4382b789c0515433f81f8f9a6fefd
AxelChaban <Axel.Chaban@ibm.com>
2023-08-29 11:37:50 +0200

    test8

--- cobol/DBBTEST.cbl
+++ cobol/DBBTEST.cbl
@@ -20,7 +20,7 @@
 ********          COPY EPSMTCOM.
        01 DATE-JOUR PIC X(10).
        PROCEDURE DIVISION.
-                MOVE AXELTESTDBB7_GITLAB07'.
+                MOVE AXELTESTDBB7_GITLAB08'.
                 PERFORM A010-AFFICHAGE-DATE.
       *
        A010-AFFICHAGE-DATE.
